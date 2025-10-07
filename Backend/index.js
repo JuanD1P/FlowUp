27d9361456a8041bb9a34ba2da9bbf4e1240389e
Juan Penagos <juanndieg2005@gmail.com>
2025-10-06 22:58:09 -0500

    CAMBIOS HOST

--- Backend/index.js
+++ Backend/index.js
@@ -10,11 +10,7 @@ import { requireAuth } from './middlewares/requireAuth.js';
 
 const app = express();
 
-app.use(cors({
-  origin: ["http://localhost:5173"],
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  credentials: true
-}));
+app.use(cors({ origin: true, credentials: true }));
 
 app.use(express.json());
 app.use(cookieParser());
